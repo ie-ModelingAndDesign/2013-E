a60255189264ed5191263ba8626984e6536c9b92
Toru NAKANDAKARI <e125726@Toru-no-MacBook-Pro.local>
2013-11-14 12:08:10 +0900

    add directory

---
+++ Toru/MyFirstApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by Toru NAKANDAKARI on 2013/11/14.
+//  Copyright (c) 2013年 Toru NAKANDAKARI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Toru/MyFirstApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyFirstApp
+//
+//  Created by Toru NAKANDAKARI on 2013/11/14.
+//  Copyright (c) 2013年 Toru NAKANDAKARI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
