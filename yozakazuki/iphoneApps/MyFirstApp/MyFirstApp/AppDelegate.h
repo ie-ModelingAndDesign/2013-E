96064193fd991491c21a606bdcdfbdd2c51847a0
Kazuki YOZA <e125714@nw1214.st.ie.u-ryukyu.ac.jp>
2013-11-14 12:16:12 +0900

    Add Apps by yoza

---
+++ yozakazuki/iphoneApps/MyFirstApp/MyFirstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Kazuki YOZA on 2013/11/13.
+//  Copyright (c) 2013年 Kazuki YOZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
