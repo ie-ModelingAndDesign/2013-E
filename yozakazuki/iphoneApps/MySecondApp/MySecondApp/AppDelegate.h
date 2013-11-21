a336e9216ab4c7e157ec6ae6526bb3737a289f78
Kazuki YOZA <e125714@Kazuki-no-MacBook-Pro.local>
2013-11-21 21:49:43 +0900

    Initial Commit

---
+++ yozakazuki/iphoneApps/MySecondApp/MySecondApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySecondApp
+//
+//  Created by Kazuki YOZA on 2013/11/21.
+//  Copyright (c) 2013年 Kazuki YOZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
