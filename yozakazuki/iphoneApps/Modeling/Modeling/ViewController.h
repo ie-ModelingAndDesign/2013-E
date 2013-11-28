d9ce6d54b374f488fbbb27725ac3e6381c2a2c54
Kazuki YOZA <e125714@Kazuki-no-MacBook-Pro.local>
2013-11-28 11:02:33 +0900

    Initial Commit

---
+++ yozakazuki/iphoneApps/Modeling/Modeling/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Modeling
+//
+//  Created by Kazuki YOZA on 2013/11/28.
+//  Copyright (c) 2013年 Kazuki YOZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ yozakazuki/iphoneApps/Modeling/Modeling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Modeling
+//
+//  Created by Kazuki YOZA on 2013/11/28.
+//  Copyright (c) 2013年 Kazuki YOZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
