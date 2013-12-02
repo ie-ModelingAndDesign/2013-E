b0ffca8d8542b24e04b1e7d07b38f7f11893173a
Kazuki YOZA <e125714@nw1214.st.ie.u-ryukyu.ac.jp>
2013-12-02 16:02:16 +0900

    atari

---
+++ Toru/MySecondApp/MySecondApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySecondApp
+//
+//  Created by Toru NAKANDAKARI on 2013/11/21.
+//  Copyright (c) 2013年 Toru NAKANDAKARI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Toru/MySecondApp/ViewController2.h
@@ -0,0 +1,16 @@
+//
+//  ViewController2.h
+//  MySecondApp
+//
+//  Created by Toru NAKANDAKARI on 2013/11/21.
+//  Copyright (c) 2013年 Toru NAKANDAKARI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController2 : UIViewController
+- (void)action;
+- (void)changeImage:(id)imageView;
+
+@end
